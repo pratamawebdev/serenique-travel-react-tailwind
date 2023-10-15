40513508997b3010b84f623b4dc42d6c90bb9b77
willy <willypratama231@gmail.com>
2023-10-15 19:44:33 +0700

    Calls useRoutes with the routes parameter and is stored in the element variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import "./App.css";
+import { useRoutes } from "react-router-dom";
 
 const routes = [{ path: "/login", element: <LoginPage /> }];
 
 function App() {
-  return (
-    <>
-      <div></div>
-    </>
-  );
+  const element = useRoutes(routes);
+  return element;
 }
 
 export default App;
