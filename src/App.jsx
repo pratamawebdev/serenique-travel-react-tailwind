66adad7e3f17479b262d51bcc61bf4a27e835108
willy <willypratama231@gmail.com>
2023-10-29 00:42:15 +0700

    Use Protected route for some page, and setting all route for page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,95 @@
 import "./App.css";
 import { useRoutes } from "react-router-dom";
 import LoginPage from "./pages/login";
+import RegisterPage from "./pages/register";
+import DashboardPage from "./pages/dashboard";
+import UserPage from "./pages/user";
+import BannerPage from "./pages/banner";
+import PromoPage from "./pages/promo";
+import NotFoundPage from "./pages/NotFound";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import CategoryPage from "./pages/category";
+import ProfilePage from "./pages/profile";
+import HomePage from "./pages/home";
+import ActivityPage from "./pages/activity";
+import ProtectedProfile from "./routes/ProtectedProfile";
 
-const routes = [{ path: "/login", element: <LoginPage /> }];
+const routes = [
+  { path: "/login", element: <LoginPage /> },
+  { path: "/register", element: <RegisterPage /> },
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <ProtectedRoute>
+        <DashboardPage />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/user",
+    element: (
+      <ProtectedRoute>
+        <UserPage />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/banner",
+    element: (
+      <ProtectedRoute>
+        <BannerPage />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/promo",
+    element: (
+      <ProtectedRoute>
+        <PromoPage />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/category",
+    element: (
+      <ProtectedRoute>
+        <CategoryPage />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/user",
+    element: (
+      <ProtectedRoute>
+        <UserPage />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/activity",
+    element: (
+      <ProtectedRoute>
+        <ActivityPage />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/profile",
+    element: (
+      <ProtectedProfile>
+        <ProfilePage />
+      </ProtectedProfile>
+    ),
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+];
 
 function App() {
   const element = useRoutes(routes);
