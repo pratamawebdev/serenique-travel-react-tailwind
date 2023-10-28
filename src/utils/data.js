9435d5d2ebddf6fa985bd03082e352c21d53fba8
willy <willypratama231@gmail.com>
2023-10-29 00:38:52 +0700

    Create and update dinamis and static data

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,2 +1,67 @@
-export const loginImage =
+export const authImage =
   "https://images.unsplash.com/photo-1600971569276-1618e55af2ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1907&q=80";
+
+export const urlStaticImage =
+  "https://images.unsplash.com/photo-1607082349566-187342175e2f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80";
+
+export const urlStaticAboutImage =
+  "https://i.pinimg.com/564x/3b/c4/b7/3bc4b71ca096a5278fa8836f9db9cb45.jpg";
+
+export const dataStatic = [
+  {
+    name: "Static",
+    imageUrl:
+      "https://i.pinimg.com/564x/3b/c4/b7/3bc4b71ca096a5278fa8836f9db9cb45.jpg",
+  },
+  {
+    name: "Static",
+    imageUrl:
+      "https://i.pinimg.com/564x/3b/c4/b7/3bc4b71ca096a5278fa8836f9db9cb45.jpg",
+  },
+  {
+    name: "Static",
+    imageUrl:
+      "https://i.pinimg.com/564x/3b/c4/b7/3bc4b71ca096a5278fa8836f9db9cb45.jpg",
+  },
+  {
+    name: "Static",
+    imageUrl:
+      "https://i.pinimg.com/564x/3b/c4/b7/3bc4b71ca096a5278fa8836f9db9cb45.jpg",
+  },
+  {
+    name: "Static",
+    imageUrl:
+      "https://i.pinimg.com/564x/3b/c4/b7/3bc4b71ca096a5278fa8836f9db9cb45.jpg",
+  },
+  {
+    name: "Static",
+    imageUrl:
+      "https://i.pinimg.com/564x/3b/c4/b7/3bc4b71ca096a5278fa8836f9db9cb45.jpg",
+  },
+];
+
+export const navItems = [
+  { title: "Home", url: "#banner" },
+  { title: "About", url: "#about" },
+  { title: "Category", url: "#category" },
+  { title: "Promo", url: "#promo" },
+  { title: "Activity", url: "#activity" },
+  { title: "Contact", url: "#contact" },
+];
+
+export const footerContentList1 = [
+  { title: "About", url: "/" },
+  { title: "Press release", url: "/" },
+  { title: "Environment", url: "/" },
+  { title: "Privacy policy", url: "/" },
+];
+
+export const footerContentList2 = [
+  { title: "Lift tickets", url: "/" },
+  { title: "Season passes", url: "/" },
+  { title: "Vacations package", url: "/" },
+  { title: "Best trips", url: "/" },
+];
+
+export const urlStaticProfileImage =
+  "https://quarantine.doh.gov.ph/wp-content/uploads/2016/12/no-image-icon-md.png";
