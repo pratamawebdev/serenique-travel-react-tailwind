de518b36599ab58ce9ef5ff0652b5f87ad0d360a
willy <willypratama231@gmail.com>
2023-10-29 00:43:07 +0700

    Update tailwind config and packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "480px",
+        sm: "640px",
+        md: "768px",
+        lg: "968px",
+        xl: "1024px",
+        "2xl": "1200px",
+      },
+      colors: {
+        primaryColor: "#015AB8",
+        darkColor: "#111827",
+      },
+      transitionDuration: {
+        400: "0.4s",
+      },
+      animation: {
+        slide: "slide 25s linear infinite",
+      },
+      keyframes: {
+        slide: {
+          "0%,100%": { transform: "translateX(5%)" },
+          "50%": { transform: "translateX(-120%)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
