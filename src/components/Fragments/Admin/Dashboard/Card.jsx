d9bf506d9b4ecf079b387837c3a0531dbbde4508
willy <willypratama231@gmail.com>
2023-11-04 14:23:09 +0700

    Update card dashboard style

--- src/components/Fragments/Admin/Dashboard/Card.jsx
+++ src/components/Fragments/Admin/Dashboard/Card.jsx
@@ -3,17 +3,17 @@ const Card = (props) => {
   const isError = count === 0;
 
   return (
-    <div className="flex items-center h-40 p-8 bg-white rounded-lg shadow-lg">
+    <div className="flex items-center h-40 p-8 bg-white rounded-lg shadow-lg text-darkColor dark:bg-gray-700">
       {children}
       <div>
         <span
           className={`block text-2xl font-bold ${
-            isError ? "text-red-500" : "text-black"
+            isError ? "text-red-500" : "text-black dark:text-white"
           }`}
         >
           {isError ? "0" : count}
         </span>
-        <span className="block text-gray-500">{title}</span>
+        <span className="block text-gray-500 dark:text-gray-300">{title}</span>
       </div>
     </div>
   );
