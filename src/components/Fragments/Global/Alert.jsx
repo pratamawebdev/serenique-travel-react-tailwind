0fa29db914762a3c5b4a937790d1fd7304a52a2f
willy <willypratama231@gmail.com>
2023-11-04 14:32:36 +0700

    Create alert

--- src/components/Fragments/Global/Alert.jsx
+++ src/components/Fragments/Global/Alert.jsx
@@ -1,5 +1,5 @@
 const Alert = (props) => {
-  const { classname, headerMessage, message } = props;
+  const { classname, headerMessage, message, onClose } = props;
   return (
     <div className={` px-4 py-3 border rounded ${classname}`} role="alert">
       <strong className="font-bold">{headerMessage}</strong>
@@ -10,6 +10,7 @@ const Alert = (props) => {
           role="button"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
+          onClick={() => onClose()}
         >
           <title>Close</title>
           <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
