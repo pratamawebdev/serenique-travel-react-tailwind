a4c8406a79597baf77f7892f90c9e9ae07e80d94
willy <willypratama231@gmail.com>
2023-11-04 14:34:12 +0700

    Change classname in loader to be props

--- src/components/Fragments/Global/Loader.jsx
+++ src/components/Fragments/Global/Loader.jsx
@@ -1,9 +1,13 @@
-const Loader = () => {
+const Loader = ({ classname }) => {
   return (
     <>
-      <div className="w-12 h-12 mx-auto border-4 border-yellow-400 border-solid rounded-full border-t-transparent animate-spin" />
+      <div
+        className={`${classname} 2 mx-auto border-4 border-yellow-400 border-solid rounded-full border-t-transparent animate-spin`}
+      />
     </>
   );
 };
+
+// w-12 h-12
 
 export default Loader;
