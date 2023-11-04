f5f9590141783e2f7044e20403848af6e77ce442
willy <willypratama231@gmail.com>
2023-11-04 14:39:42 +0700

    Make widget for pagination

--- src/components/Fragments/Global/Pagination.jsx
+++ src/components/Fragments/Global/Pagination.jsx
@@ -14,17 +14,27 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
       </button>
 
       <div className="flex gap-2">
-        {[...Array(totalPages)].map((_, index) => (
-          <button
-            key={index + 1}
-            onClick={() => handlePageClick(index + 1)}
-            className={`py-[4px] px-2 w-7 bg-blue-400 rounded ${
-              currentPage === index + 1 ? "bg-blue-600" : ""
-            }`}
-          >
-            {index + 1}
-          </button>
-        ))}
+        {[...Array(totalPages)].map((_, index) => {
+          let startPage = Math.max(1, currentPage - Math.floor(5 / 2));
+          let endPage = Math.min(totalPages, startPage + 5 - 1);
+
+          if (endPage - startPage + 1 < 5) {
+            startPage = Math.max(1, endPage - 5 + 1);
+          }
+          if (index + 1 >= startPage && index + 1 <= endPage) {
+            return (
+              <button
+                key={index + 1}
+                onClick={() => handlePageClick(index + 1)}
+                className={`py-[4px] px-2 w-7 bg-blue-400 rounded ${
+                  currentPage === index + 1 ? "bg-blue-600" : ""
+                }`}
+              >
+                {index + 1}
+              </button>
+            );
+          }
+        })}
       </div>
 
       <button
